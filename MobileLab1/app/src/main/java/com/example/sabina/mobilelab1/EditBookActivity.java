e0b48ae7d210d986b8d6e49a8807fcf51052a568
sabina1296 <alexaelenasabina@gmail.com>
2017-12-07 19:53:15 +0200

    added piechart for pages read

--- MobileLab1/app/src/main/java/com/example/sabina/mobilelab1/EditBookActivity.java
+++ MobileLab1/app/src/main/java/com/example/sabina/mobilelab1/EditBookActivity.java
@@ -10,14 +10,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditBookActivity extends AppCompatActivity {
     EditText author, title;
     //EditText title;
     Button saveButton;
     Integer position, code;
     NumberPicker np;
-
+    //String
     //Integer code;
+    String[] months={"Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"};
+    Integer[] pagesRead={0,0,12,20,37,3,0,5,97,12,21,0};
+    List<PieEntry> pieEntries=new ArrayList<PieEntry>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,6 +92,17 @@ public class EditBookActivity extends AppCompatActivity {
         np = (NumberPicker) findViewById(R.id.numberPicker);
         np.setMaxValue(2017);
         np.setMinValue(1900);
-
+        for(int i=0;i<months.length;i++)
+        {
+            pieEntries.add(new PieEntry(pagesRead[i],months[i]));
+        }
+        PieDataSet ds=new PieDataSet(pieEntries,"Pages Read this Year");
+        ds.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data=new PieData(ds);
+        PieChart chart= (PieChart) findViewById(R.id.pieChart);
+        chart.setData(data);
+        chart.invalidate();
+        chart.getLegend().setEnabled(false);
     }
+
 }
